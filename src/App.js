46ada3b589dab6d38b1c7e915dc015b66e2b5d6f
tommyrharper <tommyrharper@gmail.com>
2020-05-27 11:55:59 +0100

    Remove all unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {favoritecolor: "red"};
-  }
-  static getDerivedStateFromProps(props, state) {
-    return {favoritecolor: props.favcol };
-  }
-  changeColor = () => {
-    this.setState({favoritecolor: "blue"});
-  }
   render() {
     return (
       <div>
-      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-      <button type="button" onClick={this.changeColor}>Change color</button>
+      <h1>Hello</h1>
       </div>
     );
   }
